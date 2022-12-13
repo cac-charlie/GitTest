c253382111acdc69d096c557fed5b5a320dd6fca
Charlie Lin[林宜德] <charlie.lin@104.com.tw>
2022-12-13 16:37:19 +0800

    傳送依賴

---
+++ 104library/src/main/java/com/nuu/a104library/My104Data.kt
@@ -0,0 +1,14 @@
+package com.nuu.a104library
+
+class My104Data {
+
+    private var a = 0
+
+    fun setData(count: Int) {
+        a = count
+    }
+
+    fun getData(): Int {
+        return a
+    }
+}
--- app/src/main/java/com/nuu/gittest/MainActivity.kt
+++ app/src/main/java/com/nuu/gittest/MainActivity.kt
@@ -2,10 +2,13 @@ package com.nuu.gittest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nuu.a104library.My104Data
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        My104Data()
     }
 }
