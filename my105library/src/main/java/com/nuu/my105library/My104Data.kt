a3a5e6518967bceab8bb114429f0cb9b4f50a892
Charlie Lin[林宜德] <charlie.lin@104.com.tw>
2022-12-13 16:51:00 +0800

    傳送依賴2

---
+++ my105library/src/main/java/com/nuu/my105library/My104Data.kt
@@ -0,0 +1,14 @@
+package com.nuu.my105library
+
+class My104Data {
+
+    private var a = 0
+
+    fun setData(count: Int) {
+        a = count
+    }
+
+    fun getData(): Int {
+        return a
+    }
+}
